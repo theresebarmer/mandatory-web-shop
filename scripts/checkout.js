d51e2e63f6d255cdae9075d742cdd3e2c61a88f3
Therése Barmer <therese.barmer@gmail.com>
2018-02-26 09:05:31 +0100

    Check if first name field is empty

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,17 +1,20 @@
 let button = document.getElementById("submit");
+let validation = document.getElementById("validation");
 
-function addValidationMessage() {
-
+function addValidationMessage(input) {
+    let field = input.getAttribute("id");
+    validation.innerHTML = field + "is a required field";
 }
 
 function removeValidationMessage() {
 
 }
 
-button.addEventListener('click', function() {
+button.addEventListener('click', function(event) {
+    event.preventDefault();
     let firstName = document.getElementById("first-name");
 
-    firstName === ""
-    ? addValidationMessage()
+    firstName.value === ""
+    ? addValidationMessage(firstName)
     : removeValidationMessage();
 });
