be279acbafbcc932604d8455bba7817e8991104a
Therése Barmer <therese.barmer@gmail.com>
2018-02-26 09:31:19 +0100

    Use function for input check

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -20,37 +20,30 @@ function removeValidationMessage(input) {
     }
 }
 
+function validateField(input) {
+    input.value === ""
+        ? addValidationMessage(input)
+        : removeValidationMessage(input);
+}
+
 button.addEventListener('click', function(event) {
     event.preventDefault();
     let firstName = document.getElementById("first-name");
-
-    firstName.value === ""
-    ? addValidationMessage(firstName)
-    : removeValidationMessage(firstName);
+    validateField(firstName);
 
     let lastName = document.getElementById("last-name");
-    lastName.value === ""
-        ? addValidationMessage(lastName)
-        : removeValidationMessage(lastName);
+    validateField(lastName);
 
     let email = document.getElementById("email");
-    email.value === ""
-        ? addValidationMessage(email)
-        : removeValidationMessage(email);
+    validateField(email);
 
     let streetAddress = document.getElementById("street-address");
-    streetAddress.value === ""
-        ? addValidationMessage(streetAddress)
-        : removeValidationMessage(streetAddress);
+    validateField(streetAddress);
 
     let zipCode = document.getElementById("zip-code");
-    zipCode.value === ""
-        ? addValidationMessage(zipCode)
-        : removeValidationMessage(zipCode);
+    validateField(zipCode);
 
     let city = document.getElementById("city");
-    city.value === ""
-        ? addValidationMessage(city)
-        : removeValidationMessage(city);
+    validateField(city);
 });
 
